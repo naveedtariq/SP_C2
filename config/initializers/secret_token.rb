746f29bde2efcf693a433f63de461795ff66c177
Usman Asif <pyroxicated@gmail.com>
2012-02-15 00:02:58 +0500

    rails 3.2.1 new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SPC2::Application.config.secret_token = '9abc3fb648e2cc1f36d437e734e13638be42aa6312c02b9737f9d9380afadb45ef7e40d50c9f38792f1465eda2fb38b0251d09d23182b89ca3d12b768799b4f0'
