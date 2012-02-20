269be80b1f657f81a3c3e75c4ffb291b149274be
Usman asif <pyroxicated@gmail.com>
2012-02-20 18:24:54 +0500

    searching by month and week

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -6,13 +6,27 @@ class Ride < ActiveRecord::Base
   validates_numericality_of :available_seats, :greater_than => 0
   validates_numericality_of :price_per_seat, :greater_than_or_equal_to => 0
   validates_numericality_of :flexibility, :greater_than_or_equal_to => 0, :allow_nil => true
-#  has_one :location
+  #  has_one :location
   #accepts_nested_attributes_for :location
   # attr_accessible :from_city, :to_city, :departure_date, :departure_time, :flexibility, :duration, :ride_type, :available_seats, :price_per_seat, :notes
   def self.search_rides(params)
+    if params.blank?
+      return self.all
+    else
     params.each do |param|
       params.delete param[0] if param[1].blank?
-    end unless params.blank?
-    self.where(params)
+    end
+    end
+
+    if params[:departure_datetime]=="all"
+      self.where("from_city = ? AND to_city = ?",params[:from_city],params[:to_city])
+    else
+      if params[:departure_datetime]=="this_week"
+        dep_time = Time.now-1.week
+      else
+        dep_time = Time.now-1.month
+      end
+      self.where("from_city = ? AND to_city = ? AND departure_datetime > ?",params[:from_city],params[:to_city],dep_time)
+    end
   end
 end
