3834ece8d0165a461e75ff071b62120ed0716666
Administrator <admin@local.host>
2012-02-28 19:23:48 +0500

    Book Ride

--- spec/requests/rides_spec.rb
+++ spec/requests/rides_spec.rb
@@ -95,7 +95,38 @@ describe "Rides", :js =>true do
 #  end
 #
 #  #---------------------------------------------------------------------------------------------
-#    it "Should show Successfully Booked", :js => true do
+    it "Should show Successfully Booked", :js => true do
+    visit login_path
+    @user = Factory(:user)
+    fill_in "email", :with => @user.email
+    fill_in "password", :with => "secret"
+    click_button ('Sign in')
+    page.should have_content("Logged in!")
+    current_path.should eq(root_path)
+    click_link ('Ride input page')
+    current_path.should eq(new_ride_path)
+    select(Location.last.name, :from => "ride_from_location_id")
+    fill_in "ride_departure_date", :with => SpClock.date
+    choose('ride_ride_type_sedon')
+    fill_in "ride_total_price", :with => "60  "
+    fill_in "ride_notes", :with => "test"
+    click_button ('Create Ride')
+    page.should have_content("Successfully created ride")
+    current_path.should eq(root_path)
+    click_link ('Ride search page')
+    current_path.should eq(search_rides_path)
+    click_link ('Select')
+    click_button ('Book Ride')
+    fill_in "ride_participant_phone", :with => "4552254"
+    choose("ride_participant_mode_of_communications_phone_call")
+    click_button ('Booking Ride')
+    page.should have_content("Successfully Booked")
+    current_path.should eq(dashboard_path)
+  end
+#
+#--------------------------------- Booking Ride ------------------------------------------------
+#
+#    it "Should Successfuly ride accepted", :js => true do
 #    visit login_path
 #    @user = Factory(:user)
 #    fill_in "email", :with => @user.email
@@ -103,169 +134,148 @@ describe "Rides", :js =>true do
 #    click_button ('Sign in')
 #    page.should have_content("Logged in!")
 #    current_path.should eq(root_path)
+#    click_link ('Ride input page')
+#    current_path.should eq(new_ride_path)
+#    select(Location.last.name, :from => "ride_from_location_id")
+#    fill_in "ride_departure_date", :with => SpClock.date
+#    choose('ride_ride_type_sedon')
+#    fill_in "ride_total_price", :with => "60  "
+#    fill_in "ride_notes", :with => "test"
+#    click_button ('Create Ride')
+#    page.should have_content("Successfully created ride")
+#    current_path.should eq(root_path)
+#    click_link('Log out')
+#    current_path.should eq(login_path)
+#    @user2 = Factory(:user)
+#    fill_in "email", :with => @user2.email
+#    fill_in "password", :with => "secret"
+#    click_button ('Sign in')
+#    page.should have_content("Logged in!")
 #    click_link ('Ride search page')
 #    current_path.should eq(search_rides_path)
 #    click_link ('Select')
+#    current_path.should eq('/rides/1/ride_participants/new')
 #    click_button ('Book Ride')
 #    fill_in "ride_participant_phone", :with => "4552254"
 #    choose("ride_participant_mode_of_communications_phone_call")
 #    click_button ('Booking Ride')
 #    page.should have_content("Successfully Booked")
 #    current_path.should eq(dashboard_path)
+#    click_link('Log out')
+#    current_path.should eq(login_path)
+#    fill_in "email", :with => @user.email
+#    fill_in "password", :with => "secret"
+#    click_button ('Sign in')
+#    page.should have_content("Logged in!")
+#    current_path.should eq(root_path)
+#    click_link ('My Dashboard')
+#    current_path.should eq(dashboard_path)
+#    #click_link ('Accept')
+#    visit '/rides/1/ride_participants/1/accept'
+#    current_path.should eq('/rides/1/ride_participants/1/accept')
+#  end
+##------------------------------ Accept Ride -----------------------------------------------------
+#
+#      it "Should Successfuly ride denied", :js => true do
+#    visit login_path
+#    @user = Factory(:user)
+#    fill_in "email", :with => @user.email
+#    fill_in "password", :with => "secret"
+#    click_button ('Sign in')
+#    page.should have_content("Logged in!")
+#    current_path.should eq(root_path)
+#    click_link ('Ride input page')
+#    current_path.should eq(new_ride_path)
+#    select(Location.last.name, :from => "ride_from_location_id")
+#    fill_in "ride_departure_date", :with => SpClock.date
+#    choose('ride_ride_type_sedon')
+#    fill_in "ride_total_price", :with => "60  "
+#    fill_in "ride_notes", :with => "test"
+#    click_button ('Create Ride')
+#    page.should have_content("Successfully created ride")
+#    current_path.should eq(root_path)
+#    click_link('Log out')
+#    current_path.should eq(login_path)
+#    @user2 = Factory(:user)
+#    fill_in "email", :with => @user2.email
+#    fill_in "password", :with => "secret"
+#    click_button ('Sign in')
+#    page.should have_content("Logged in!")
+#    click_link ('Ride search page')
+#    current_path.should eq(search_rides_path)
+#    click_link ('Select')
+#    current_path.should eq('/rides/1/ride_participants/new')
+#    click_button ('Book Ride')
+#    fill_in "ride_participant_phone", :with => "4552254"
+#    choose("ride_participant_mode_of_communications_phone_call")
+#    click_button ('Booking Ride')
+#    page.should have_content("Successfully Booked")
+#    current_path.should eq(dashboard_path)
+#    click_link('Log out')
+#    current_path.should eq(login_path)
+#    fill_in "email", :with => @user.email
+#    fill_in "password", :with => "secret"
+#    click_button ('Sign in')
+#    page.should have_content("Logged in!")
+#    current_path.should eq(root_path)
+#    click_link ('My Dashboard')
+#    current_path.should eq(dashboard_path)
+#    #click_link ('Deny')
+#    visit '/rides/1/ride_participants/1/deny'
+#    current_path.should eq('/rides/1/ride_participants/1/deny')
 #  end
 #
-#--------------------------------- Booking Ride ------------------------------------------------
+## ----------------------------- Deny Ride -------------------------------------------------------
+##
+#   it "Should Cancel the ride successfully", :js => true do
+#    visit login_path
+#    @user = Factory(:user)
+#    fill_in "email", :with => @user.email
+#    fill_in "password", :with => "secret"
+#    click_button ('Sign in')
+#    page.should have_content("Logged in!")
+#    current_path.should eq(root_path)
+#    click_link ('Ride input page')
+#    current_path.should eq(new_ride_path)
+#    select(Location.last.name, :from => "ride_from_location_id")
+#    fill_in "ride_departure_date", :with => SpClock.date
+#    choose('ride_ride_type_sedon')
+#    fill_in "ride_total_price", :with => "60  "
+#    fill_in "ride_notes", :with => "test"
+#    click_button ('Create Ride')
+#    current_path.should eq(root_path)
+#    click_link ('My Dashboard')
+#    current_path.should eq(dashboard_path)
+#    click_link ('Cancel ride')
+#    current_path.should eq(dashboard_path)
+#   end
+##-------------------------------- Cancel Ride -------------------------------------------------
 #
-    it "Should Successfuly ride accepted", :js => true do
-    visit login_path
-    @user = Factory(:user)
-    fill_in "email", :with => @user.email
-    fill_in "password", :with => "secret"
-    click_button ('Sign in')
-    page.should have_content("Logged in!")
-    current_path.should eq(root_path)
-    click_link ('Ride input page')
-    current_path.should eq(new_ride_path)
-    select(Location.last.name, :from => "ride_from_location_id")
-    fill_in "ride_departure_date", :with => SpClock.date
-    choose('ride_ride_type_sedon')
-    fill_in "ride_total_price", :with => "60  "
-    fill_in "ride_notes", :with => "test"
-    click_button ('Create Ride')
-    page.should have_content("Successfully created ride")
-    current_path.should eq(root_path)
-    click_link('Log out')
-    current_path.should eq(login_path)
-    @user2 = Factory(:user)
-    fill_in "email", :with => @user2.email
-    fill_in "password", :with => "secret"
-    click_button ('Sign in')
-    page.should have_content("Logged in!")
-    click_link ('Ride search page')
-    current_path.should eq(search_rides_path)
-    click_link ('Select')
-    current_path.should eq('/rides/1/ride_participants/new')
-    click_button ('Book Ride')
-    fill_in "ride_participant_phone", :with => "4552254"
-    choose("ride_participant_mode_of_communications_phone_call")
-    click_button ('Booking Ride')
-    page.should have_content("Successfully Booked")
-    current_path.should eq(dashboard_path)
-    click_link('Log out')
-    current_path.should eq(login_path)
-    fill_in "email", :with => @user.email
-    fill_in "password", :with => "secret"
-    click_button ('Sign in')
-    page.should have_content("Logged in!")
-    current_path.should eq(root_path)
-    click_link ('My Dashboard')
-    current_path.should eq(dashboard_path)
-    #click_link ('Accept')
-    visit '/rides/1/ride_participants/1/accept'
-    current_path.should eq('/rides/1/ride_participants/1/accept')
-  end
-#------------------------------ Accept Ride -----------------------------------------------------
-
-      it "Should Successfuly ride denied", :js => true do
-    visit login_path
-    @user = Factory(:user)
-    fill_in "email", :with => @user.email
-    fill_in "password", :with => "secret"
-    click_button ('Sign in')
-    page.should have_content("Logged in!")
-    current_path.should eq(root_path)
-    click_link ('Ride input page')
-    current_path.should eq(new_ride_path)
-    select(Location.last.name, :from => "ride_from_location_id")
-    fill_in "ride_departure_date", :with => SpClock.date
-    choose('ride_ride_type_sedon')
-    fill_in "ride_total_price", :with => "60  "
-    fill_in "ride_notes", :with => "test"
-    click_button ('Create Ride')
-    page.should have_content("Successfully created ride")
-    current_path.should eq(root_path)
-    click_link('Log out')
-    current_path.should eq(login_path)
-    @user2 = Factory(:user)
-    fill_in "email", :with => @user2.email
-    fill_in "password", :with => "secret"
-    click_button ('Sign in')
-    page.should have_content("Logged in!")
-    click_link ('Ride search page')
-    current_path.should eq(search_rides_path)
-    click_link ('Select')
-    current_path.should eq('/rides/1/ride_participants/new')
-    click_button ('Book Ride')
-    fill_in "ride_participant_phone", :with => "4552254"
-    choose("ride_participant_mode_of_communications_phone_call")
-    click_button ('Booking Ride')
-    page.should have_content("Successfully Booked")
-    current_path.should eq(dashboard_path)
-    click_link('Log out')
-    current_path.should eq(login_path)
-    fill_in "email", :with => @user.email
-    fill_in "password", :with => "secret"
-    click_button ('Sign in')
-    page.should have_content("Logged in!")
-    current_path.should eq(root_path)
-    click_link ('My Dashboard')
-    current_path.should eq(dashboard_path)
-    #click_link ('Deny')
-    visit '/rides/1/ride_participants/1/deny'
-    current_path.should eq('/rides/1/ride_participants/1/deny')
-  end
-
-# ----------------------------- Deny Ride -------------------------------------------------------
-#
-   it "Should Cancel the ride successfully", :js => true do
-    visit login_path
-    @user = Factory(:user)
-    fill_in "email", :with => @user.email
-    fill_in "password", :with => "secret"
-    click_button ('Sign in')
-    page.should have_content("Logged in!")
-    current_path.should eq(root_path)
-    click_link ('Ride input page')
-    current_path.should eq(new_ride_path)
-    select(Location.last.name, :from => "ride_from_location_id")
-    fill_in "ride_departure_date", :with => SpClock.date
-    choose('ride_ride_type_sedon')
-    fill_in "ride_total_price", :with => "60  "
-    fill_in "ride_notes", :with => "test"
-    click_button ('Create Ride')
-    current_path.should eq(root_path)
-    click_link ('My Dashboard')
-    current_path.should eq(dashboard_path)
-    click_link ('Cancel ride')
-    current_path.should eq(dashboard_path)
-   end
-#-------------------------------- Cancel Ride -------------------------------------------------
-
-    it "Should show Ride was successfully updated.", :js => true do
-    visit login_path
-    @user = Factory(:user)
-    fill_in "email", :with => @user.email
-    fill_in "password", :with => "secret"
-    click_button ('Sign in')
-    page.should have_content("Logged in!")
-    current_path.should eq(root_path)
-    click_link ('Ride input page')
-    current_path.should eq(new_ride_path)
-    select(Location.last.name, :from => "ride_from_location_id")
-    fill_in "ride_departure_date", :with => SpClock.date
-    choose('ride_ride_type_sedon')
-    fill_in "ride_total_price", :with => "60  "
-    fill_in "ride_notes", :with => "test"
-    click_button ('Create Ride')
-    current_path.should eq(root_path)
-    click_link ('My Dashboard')
-    current_path.should eq(dashboard_path)
-    click_link ('Modify ride')
-    current_path.should eq('/rides/2/edit')
-    click_button ('Create Ride')
-    current_path.should eq(dashboard_path)
-    page.should have_content("Ride was successfully updated.")
-   end
+#    it "Should show Ride was successfully updated.", :js => true do
+#    visit login_path
+#    @user = Factory(:user)
+#    fill_in "email", :with => @user.email
+#    fill_in "password", :with => "secret"
+#    click_button ('Sign in')
+#    page.should have_content("Logged in!")
+#    current_path.should eq(root_path)
+#    click_link ('Ride input page')
+#    current_path.should eq(new_ride_path)
+#    select(Location.last.name, :from => "ride_from_location_id")
+#    fill_in "ride_departure_date", :with => SpClock.date
+#    choose('ride_ride_type_sedon')
+#    fill_in "ride_total_price", :with => "60  "
+#    fill_in "ride_notes", :with => "test"
+#    click_button ('Create Ride')
+#    current_path.should eq(root_path)
+#    click_link ('My Dashboard')
+#    current_path.should eq(dashboard_path)
+#    click_link ('Modify ride')
+#    current_path.should eq('/rides/2/edit')
+#    click_button ('Create Ride')
+#    current_path.should eq(dashboard_path)
+#    page.should have_content("Ride was successfully updated.")
+#   end
 #------------------------------------- Modify Ride ------------------------------------------------
 
 end
