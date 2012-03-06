3518377a8d1bbefd2d4f76caa3481bcb4e74d1c4
Usman Asif <pyroxicated@gmail.com>
2012-03-07 00:41:25 +0500

    phase 4

--- db/migrate/20120306162604_departure_date_default.rb
+++
@@ -1,7 +0,0 @@
-class DepartureDateDefault < ActiveRecord::Migration
-  def change
-    change_column :rides, :duration_in_minutes, :default => 900
-    change_column :rides, :flexibility_in_minutes, :default => 900
-  end
-
-end
