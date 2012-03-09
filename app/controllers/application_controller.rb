05407713c847b47833365a09b133d0a2c8bd44a2
Usman Asif <pyroxicated@gmail.com>
2012-03-10 00:37:52 +0500

    final build

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   before_filter :save_request
   helper_method :production?
   def save_request
-#    return render :text => request.cookies
     RequestLogger.create!(:params => request.inspect, :request_url => request.url, :user_agent => request.user_agent, :ip_address => request.remote_ip, :referer => request.referer, :cookie_string => request.cookies)
   end
   def not_authenticated
