be7eb5af261cc9bcfa26c6f9c51dcf249c126f54
Administrator <admin@local.host>
2012-03-12 19:00:11 +0500

    404ErrorPages+TOGGLE+Merge Branch

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -33,3 +33,43 @@ GOOGLE_ANALYTICS = "<script type=\"text/javascript\">
   })();
 
 </script>"
+
+MIXPANEL = "<script type=\"text/javascript\">
+var mpq=[];mpq.push(['init', 'dc892638d7e815ceb9e257c44d6c4a05']);
+(function(){var b,a,e,d,c;b=document.createElement('script');
+b.type='text/javascript';
+b.async=true;
+b.src=(document.location.protocol=== 'https:'?'https:':'http:')+'api.mixpanel.com/site_media/js/api/mixpanel.js';
+a=document.getElementsByTagName('script')[0];
+a.parentNode.insertBefore(b,a);
+e=function(f){return function(){mpq.push([f].concat(Array.prototype.slice.call(arguments,0)))}};
+d=['init','track','track_links','track_forms','register','register_once','identify','name_tag','set_config'];
+for(c=0;c<d.length;c++){mpq[d[c]]=e(d[c])}})();
+</script>"
+
+GETCLICKY = "<script src='static.getclicky.com/js' type='text/javascript'></script>
+<script type='text/javascript'>try{ clicky.init(66553995); }catch(e){}</script>
+<noscript><p><img alt='Clicky' width='1' height='1' src='in.getclicky.com/66553995ns.gif' /></p></noscript>"
+
+KISSMETRICS = "<script type='text/javascript'>
+    var _kmq = _kmq || [];
+    function _kms(u){
+    setTimeout(function(){
+    var s = document.createElement('script'); var f = document.getElementsByTagName('script')[0]; s.type = 'text/javascript'; s.async = true;
+    s.src = u; f.parentNode.insertBefore(s, f);
+    }, 1);
+    }
+    _kms('//i.kissmetrics.com/i.js');_kms('//doug1izaerwt3.cloudfront.net/76b6b624ef34a2ff61ef145f5b92b28af2ea8b8c.1.js');
+    </script>"
+
+  CRAZYEGG = ""
+
+  GOOGLE_ANALYTICS_TOGGLE = "ON"
+  MIXPANEL_TOGGLE = "ON"
+  GETCLICKY_TOGGLE = "ON"
+  KISSMETRICS_TOGGLE = "ON"
+  CRAZYEGG_TOGGLE = "ON"
+
+
+
+
