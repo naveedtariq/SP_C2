0e76177e7fce6f2977f5960b1a6fde6559e2806f
Ubuntu <ubuntu@domU-12-31-38-00-2C-02.compute-1.internal>
2012-03-22 14:07:34 +0000

    foriegn key

---
+++ config/unicorn.rb
@@ -0,0 +1,24 @@
+source_folder = "/home/ubuntu/SP_C2"
+worker_processes 2
+working_directory source_folder
+
+# This loads the application in the master process before forking
+# worker processes
+# Read more about it here:
+# http://unicorn.bogomips.org/Unicorn/Configurator.html
+preload_app true
+
+timeout 30
+
+# This is where we specify the socket.
+# We will point the upstream Nginx module to this socket later on
+#listen "127.0.0.1:9001", :backlog => 64
+listen "#{source_folder}/tmp/sockets/unicorn.sock", :backlog => 64
+
+pid "#{source_folder}/tmp/SP_C2_unicorn.pid"
+
+# Set the path of the log files inside the log folder of the testapp
+stderr_path "#{source_folder}/log/unicorn.stderr.log"
+stdout_path "#{source_folder}/log/unicorn.stdout.log"
+
+
