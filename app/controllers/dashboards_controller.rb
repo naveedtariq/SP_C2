82013bd925707fd1e6d6542b225c976726e0c73c
Usman <pyroxicated@gmail.com>
2012-04-11 00:48:03 +0500

    small fixes

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,8 @@
 class DashboardsController < ApplicationController
   before_filter :require_login
   def show
-    @ride_participants = current_user.ride_participants.current_rides.active_rides.role_wise
-    @past_rides = current_user.ride_participants.past_rides.active_rides.paginate(:page => params[:past_rides_page], :per_page => PAST_RIDES_PER_PAGE)
+    @ride_participants = current_user.ride_participants.current_rides.active_rides.not_cancelled_or_abandoned.role_wise
+    @past_rides = current_user.ride_participants.past_rides.active_rides.confirmed_or_owner_participants.paginate(:page => params[:past_rides_page], :per_page => PAST_RIDES_PER_PAGE)
    
   end
 end
