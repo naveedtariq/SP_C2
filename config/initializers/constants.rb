0f6db2e083ac91dbaaa2c3f3578a735f07cdfdfb
Usman <pyroxicated@gmail.com>
2012-04-19 21:12:06 +0500

    utc

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -47,7 +47,7 @@ d=['init','track','track_links','track_forms','register','register_once','identi
 for(c=0;c<d.length;c++){mpq[d[c]]=e(d[c])}})();
 </script>"
 
-GETCLICKY = "<script src='static.getclicky.com/js' type='text/javascript'></script>
+GETCLICKY = "<script src='http://static.getclicky.com/js' type='text/javascript'></script>
 <script type='text/javascript'>try{ clicky.init(66553995); }catch(e){}</script>
 <noscript><p><img alt='Clicky' width='1' height='1' src='in.getclicky.com/66553995ns.gif' /></p></noscript>"
 
