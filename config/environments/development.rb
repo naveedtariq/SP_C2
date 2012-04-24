232eb7efe797c0cb7cb96ccf10b90148fdea7f31
Administrator <admin@local.host>
2012-04-24 19:52:25 +0500

    Resolved

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ SPC2::Application.configure do
 
   config.middleware.use ExceptionNotifier,
                         sender_address: 'spnotification00@gmail.com',
-                        exception_recipients: ['pyroxicated@gmail.com']
+                        exception_recipients: ['junaidg27@hotmail.com','bilal.basharat@devsinc.com','usman.asif@devsinc.com','junaid.qayyum@devsinc.com']
   #ignore_exceptions: ExceptionNotifier.default_ignore_exceptions # + [RuntimeError]
 
   #config.action_mailer.delivery_method = :letter_opener
