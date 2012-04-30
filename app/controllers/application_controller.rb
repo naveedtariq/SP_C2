4c74b8fde66bf69c800dd1ee8a6534bbb89ae4c8
Usman <pyroxicated@gmail.com>
2012-04-30 22:01:50 +0500

    error fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,21 +29,20 @@ class ApplicationController < ActionController::Base
   end
 
   def store_ride(ride) # To store the ride information
+
     ride_departure_date = ride.departure_date.to_s
-    ride_return_trip_departure_date= ride.return_trip_departure_date.to_s
-    ride_return_trip_departure_time= ride.return_trip_departure_time.to_s
-    ride_return_trip_checkbox= ride.return_trip_checkbox.to_s
-#    cookies["return_trip_departure_date"] = ride.return_trip_departure_date
-#    cookies["return_trip_departure_time"] = ride.return_trip_departure_time
+    ride_return_trip_departure_date = ride.return_trip_departure_date.to_s
+    ride_return_trip_departure_time = ride.return_trip_departure_time.to_s
+    ride_return_trip_checkbox = ride.return_trip_checkbox.to_s
     ride_attributes = ride.attributes
     ride_attributes.delete("departure_date")
-#    ride_attributes.delete("return_trip_departure_date")
-#    ride_attributes.delete("return_trip_departure_time")
     ride_attributes.delete("return_trip_checkbox")
     ride_attributes.delete("departuredatetime")
     ride_attributes.delete("created_at")
     ride_attributes.delete("updated_at")
-    cookies[:ride] = "#{ride_attributes.to_s}, return_trip_checkbox=>#{ride_return_trip_checkbox}, return_trip_departure_time=>#{ride_return_trip_departure_time}, departure_date=>#{ride_departure_date}, return_trip_departure_date=>#{ride_return_trip_departure_date}".gsub!("nil", "")
+
+    cookies[:ride] = "#{ride_attributes.to_s}, return_trip_checkbox=>#{ride_return_trip_checkbox}, return_trip_departure_time=>#{ride_return_trip_departure_time}, departure_date=>#{ride_departure_date}, return_trip_departure_date=>#{ride_return_trip_departure_date}".gsub(/^nil|\\r|\\n$/, "")
+
   end
 
   def store_ride_participants(ride_participant) # Ride Participant store in cookies
