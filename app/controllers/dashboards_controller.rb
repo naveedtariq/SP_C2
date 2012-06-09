ad4a9415488cd90ff114426927162c7764bbf77f
Naveed Tariq <naved.tariq@gmail.com>
2012-06-09 21:33:15 +0500

    dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,8 +3,6 @@ class DashboardsController < ApplicationController
   def show                                  # function to show the current and past rides of current user
     @ride_participants = current_user.ride_participants.current_rides(SpClock.time).active_rides.not_cancelled_or_abandoned.role_wise             # Retrieve the current user rides which are active
     @past_rides = current_user.ride_participants.past_rides(SpClock.time).active_rides.confirmed_or_owner_participants.paginate(:page => params[:past_rides_page], :per_page => PAST_RIDES_PER_PAGE)    # Retrieve the current user past rides those rides which are confirmed or current user is owner of ride
-
     @messages = current_user.owned_messages.rev_ordered  # Retrieve the current messages in inbox by the current user
-   
   end
 end
